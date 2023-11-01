5e7a6665ef2dc910e94156290aa467ad158f8829
unknown <galangotnaites@gmail.com>
2023-11-01 16:41:35 +0700

    update rekap

--- app/Http/Controllers/DashboardController copy.php
+++ app/Http/Controllers/DashboardController copy.php
@@ -30,31 +30,20 @@ class DashboardController extends Controller
         $data['sub_judul'] = "Rekap Data";
         $data['dinas'] = DB::select("SELECT id_pegawai, nama, nip FROM pegawai");
         foreach ($data['dinas'] as $p) {
-            // $dinas = DB::select("SELECT keterangan, DAY(tanggal) tgl, DAY(tanggal_pulang) tgl_p FROM dinas WHERE pegawai_id='4'");
             $dinas = DB::select("SELECT keterangan, DAY(tanggal) tgl, DAY(tanggal_pulang) tgl_p FROM dinas WHERE pegawai_id='$p->id_pegawai'");
-            // return $dinas[0]->tgl;
+            $selisih = $dinas[0]->tgl-$dinas[0]->tgl_p;
             $p->dinas = [];
             for ($i=0; $i < $data['jml_hari']; $i++) { 
-                $a['x'.$i+1] = "0";
-
-            }
-
-            foreach ($dinas as $d) {
-                $a['x'.$d->tgl] = $d->keterangan;
-                $a['x'.$d->tgl_p] = $d->keterangan;
-                $sel = $d->tgl_p-$d->tgl;
-                if($sel != 0) {
-                    for ($x=1; $x < $sel; $x++) { 
-                        if ($d->tgl+1 <= $d->tgl_p){
-                            $a['x'.$d->tgl+$x] = $d->keterangan;
-                        }
-                    }
+                // return $dinas[$i]->tgl;
+                if(isset($dinas[$i]->tgl) && $dinas[$i]->tgl=$i+1) {
+                    $a['x'.$i+1] = $dinas[$i]->keterangan;
+                } else {
+                    $a['x'.$i+1] = "0";
                 }
             }
-
             array_push($p->dinas, (object) $a);
         }       
-        // return $data['dinas'];
+        return $data['dinas'];
         // return $data['dinas'][0]->dinas[0]->x1;
         return view('rekap.rekap', $data);
     }
---
+++ resources/views/rekap/rekap.blade.php
@@ -0,0 +1,74 @@
+@extends('layouts.template')
+@section('content')
+<div id="kt_content_container" class="d-flex flex-column-fluid align-items-start container-xxl">
+    <!--begin::Post-->
+    <div class="content col-12" id="kt_content">
+		<div class="card">
+			<!--begin::Datatable-->
+			<div class="card-body pt-0">
+                <div class="fv-row mt-10 mb-10">
+                    <table class="table table-row-bordered gy-5" id="data_table" width="100%">
+                        <thead>
+                            <tr class="fw-bold fs-6 text-muted">
+                                <th width="3%">No</th>
+                                <th width="30%">Nama Pegawai</th>
+                                @for ($i = 1; $i < $jml_hari; $i++)
+                                <th>{{$i}}</th>
+                                @endfor
+                                <th>DD</th>
+                                <th>DL</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            @php
+                                $no=1;
+                            @endphp
+                            @foreach ($dinas as $p)
+                            @php
+                                $dd = 0;
+                                $dl = 0;
+                            @endphp
+                            <tr>
+                                <td>{{$no++}}</td>
+                                <td>{{$p->nama}}</td>
+                                @for ($i = 1; $i < $jml_hari; $i++)
+                                @php
+                                    $x = 'x'.$i;
+                                @endphp
+                                @if ($p->dinas[0]->$x == 'Dinas Luar')
+                                @php
+                                    $dl = $dl+1
+                                @endphp
+                                <td><span class="badge badge-danger">DL</span></td>                            
+                                @elseif ($p->dinas[0]->$x == 'Dinas Dalam')
+                                @php
+                                    $dd = $dd+1
+                                @endphp
+                                <td><span class="badge badge-info">DD</span></td>                            
+                                @else
+                                <td>-</td>                            
+                                @endif
+                                @endfor
+                                <td>{{$dd}}</td>
+                                <td>{{$dl}}</td>
+                            </tr>
+                            @endforeach
+                        </tbody>
+                    </table>
+                </div>
+			</div>
+			<br>
+			<!--end::Datatable-->
+		</div>
+    </div>
+    <!--end::Post-->
+</div>
+@endsection
+@section('js')
+<script>
+    $('#data_table').dataTable( {
+    "searching": true,
+    "scrollX": true
+} );
+</script>
+@endsection
